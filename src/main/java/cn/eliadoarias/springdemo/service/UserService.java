c672e180f42d85cd21ef38e01e3a851510a17583
EliadOArias <2144359102@qq.com>
2025-09-11 11:30:34 +0800

    配置文件更新

--- src/main/java/cn/eliadoarias/springdemo/service/UserService.java
+++ src/main/java/cn/eliadoarias/springdemo/service/UserService.java
@@ -2,6 +2,6 @@ package cn.eliadoarias.springdemo.service;
 
 
 public interface UserService {
-    public Integer login(String username,String password);
-    public Integer register(String username,String password, String name, Integer userType);
+    Integer login(String username, String password);
+    Integer register(String username, String password, String name, Integer userType);
 }
--- src/main/java/cn/eliadoarias/springdemo/util/JwtUtil.java
+++ src/main/java/cn/eliadoarias/springdemo/util/JwtUtil.java
@@ -1,9 +1,5 @@
 package cn.eliadoarias.springdemo.util;
 
-import io.jsonwebtoken.security.Keys;
-
-import java.security.Key;
-
 public class JwtUtil {
 
 }
